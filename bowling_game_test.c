2f0668a066c7c36fb2bec11d107d8f110933b4d1
nelsonmatenda <matendafigueiredo@hotmail.com>
2025-01-11 14:38:10 +0000

    Terceiro teste
    segundo o exercicio ao implementar o terceiro teste algumas coisas
    parecem não estarem certas. Logo quando algo assim acontece no TDD
    nós devemos para com o teste atual, voltar para o estado do teste
    anterior e repensar na nossa solução

--- bowling_game_test.c
+++ bowling_game_test.c
@@ -6,7 +6,7 @@
 /*   By: matenda <matenda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 12:50:40 by matenda           #+#    #+#             */
-/*   Updated: 2025/01/11 14:22:54 by matenda          ###   ########.fr       */
+/*   Updated: 2025/01/11 14:33:25 by matenda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,20 @@ static void	test_all_ones(void)
 			&& "test_all_ones()");
 }
 
+static void	test_one_spare(void)
+{
+	bowling_game_init();
+	bowling_game_roll(5);
+	bowling_game_roll(5);
+	bowling_game_roll(3);
+	roll_many(17, 0);
+	assert(bowling_game_score() == 16 \
+			&& "test_one_spare()");
+}
+
 int	main(void)
 {
 	test_gutter_game();
 	test_all_ones();
+	test_one_spare();
 }
